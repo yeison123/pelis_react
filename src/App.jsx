2f45867a8b058b846588f64a549e4031abb3a78a
Yeison Cardenas <yeison_123.2@hotmail.com>
2024-10-19 13:04:06 -0500

    Agregando comentarios al código

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {Movies} from './components/Movies.jsx';
 import {useEffect, useState} from 'react';
 
 // Comentario de Incio
+// Comentario de Incio2
 
 function App() {
   const {movies} = useMovies()
